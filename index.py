dda72279fa932b7ae7129e0e4dac245301b210e8
Dyego_bernardo <dyegobernardo23@gmail.com>
2023-10-04 11:41:47 -0300

    correção do código

--- index.py
+++ index.py
@@ -1,24 +1,25 @@
-# Variáveis de entrada
-nome_do_heroi = input("Digite o nome do herói: ")  # Solicita o nome do herói ao usuário
-xp_do_heroi = int(input("Digite a quantidade de experiência do herói: "))  # Solicita a experiência do herói ao usuário e converte para inteiro
+heroi = input("Digite o nome do herói:" )
+xp = int(input("Digite a quantidade de experiência do herói:" ))
 
-# Estrutura de decisão para determinar o nível do herói
-if xp_do_heroi < 1000:
-    nivel_do_heroi = "Ferro"
-elif 1001 <= xp_do_heroi <= 2000:
-    nivel_do_heroi = "Bronze"
-elif 2001 <= xp_do_heroi <= 5000:
-    nivel_do_heroi = "Prata"
-elif 6001 <= xp_do_heroi <= 7000:
-    nivel_do_heroi = "Ouro"
-elif 7001 <= xp_do_heroi <= 8000:
-    nivel_do_heroi = "Platina"
-elif 8001 <= xp_do_heroi <= 9000:
-    nivel_do_heroi = "Ascendente"
-elif 9001 <= xp_do_heroi <= 10000:
-    nivel_do_heroi = "Imortal"
-else:
-    nivel_do_heroi = "Radiante"
+if xp <1000:
+     nivel = "Ferro"
+elif 1000 <= xp <=2000:
+     nivel = "Bronze"
+elif 2000 <= xp <=5000:
+     nivel = "Prata"
+elif 6000 <= xp <=7000:
+     nivel = "Ouro"
+elif 7000 <= xp <= 8000:
+     nivel = "Platina"
+elif 8000 <= xp <= 9000:
+     nivel = "Ascendente"   
+elif 9000 <= xp <= 10000:
+     nivel = "imortal"
+elif 10001 <= xp:
+     nivel = "Radiante" 
+     
 
-# Saída
-print(f"O Herói de nome {nome_do_heroi} está no nível de {nivel_do_heroi}")
+print(f"O Herói de nome {heroi} está no nível de {nivel}")
+
+         
+
